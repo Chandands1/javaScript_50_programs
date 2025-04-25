9a868102addf95bbbcc17a9b1bd9de2d08d9176c
chandan_ds <chandandhanush273747@gmail.com>
2025-04-25 21:07:07 +0530

    more codes

---
+++ type_conversion.js
@@ -0,0 +1,18 @@
+//number to string conversion
+let number =123;
+let str = String(number);
+console.log(typeof str);
+
+
+//string to number conversion
+
+let str1 = "234";
+let num = Number(str1);
+console.log(typeof num);
+
+//Boolean Conversion
+let val =0;
+let bool = Boolean(val);
+console.log(bool);
+
+
